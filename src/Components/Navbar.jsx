168a8efb0a984e43de8b70d6ea9a6b28abf5432d
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-05-02 22:43:18 +0530

    navbar is created

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Style.css'
+
+const Navbar = () => {
+  return (
+    <div className="">
+    <header>
+        <nav className='pj-navbar'>
+    <h3 className='logo'>Nakul's Store</h3>
+    <ul className='pj-list'>
+      <li>Home</li>
+      <li>Store</li>
+      <li>Contact</li>
+      <li>About</li>
+
+    </ul>
+    
+    
+  </nav>
+  </header>
+  
+  </div>
+  )
+}
+
+export default Navbar
