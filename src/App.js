92277edeb36d4f9f529a03c6c32205d191a78fac
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-05-06 20:56:27 +0530

    Adding the curvaceous Homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import About from './Components/About/About';
 import Home from './Components/Home/Home';
 
 import Navbar from './Components/Navbar';
@@ -10,6 +11,7 @@ function App() {
 
     <Navbar />
     <Home />
+    <About />
       
          
     </div>
