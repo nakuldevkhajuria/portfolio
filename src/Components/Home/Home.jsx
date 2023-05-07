d7a94683e3119f533125528bb20bc2bd72b4164b
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-05-07 23:07:40 +0530

    computer icon added using font awsome

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,13 +6,15 @@ const Home = () => {
             <div className='container'>
                 <div className='content'>
                     <div className='hero-text'>
-                        <h1>Front End React Developer</h1>
+                        <h1>Front End React Developer
+                        <i class="fa-solid fa-display"></i>
+                        </h1>
                         <p>
                             Hi, i'm Nakul dev Khajuria. A passionate Front-end React developer based in Delhi, India.
                         </p>
                         <div className='social-media'>
-                            <a aria-label="linkedin" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/stefan-topalovic-dev/"><i class="fa-brands fa-linkedin"></i></a>
-                            <a aria-label="github" rel="noreferrer" target="_blank" href="https://github.com/stefvndev"><i class="fa-brands fa-github"></i></a>
+                            <a aria-label="linkedin" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/nakuldevkhajuria/"><i class="fa-brands fa-linkedin"></i></a>
+                            <a aria-label="github" rel="noreferrer" target="_blank" href="https://github.com/nakuldevkhajuria"><i class="fa-brands fa-github"></i></a>
 
                         </div>
                     </div>
