66954b58d7affa05467f195ac59d7a6be576244f
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-05-08 18:14:00 +0530

    transition from height 0 to auto with CSS using grid and grid-template-rows

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -2,23 +2,45 @@ import React from 'react'
 import './Style.css'
 const Project = () => {
   return (
+    <>
     <div className='section'> 
-       <section className='blue'>
     <h1>Project 1</h1>
+       <section id='grid' className='blue'>
+        <div>
+       
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi sed facere esse animi expedita aut iure consequuntur alias sint. Deserunt quibusdam voluptas doloribus libero facilis cum molestiae praesentium, ratione ipsa.</p>
- 
+    </div>
 </section>
-<section className='orange'>
-    <h1>Project 2</h1>
+<h1>Project 2</h1>
+<section id='grid' className='orange'>
+  <div>
+   
     <p>Voluptas, a voluptates. Eum, qui unde ipsum illum earum assumenda voluptates animi distinctio voluptatum odit non provident dolore nam dolores blanditiis, impedit in, alias ad reprehenderit cumque commodi necessitatibus corporis.</p>
-    
+    </div>
 
 </section>
-<section className='pink'>
-    <h1>Project 3</h1>
+<h1>Project 3</h1>
+<section id='grid' className='pink'>
+
+  <div>
+    
     <p>Voluptas, a voluptates. Eum, qui unde ipsum illum earum assumenda voluptates animi distinctio voluptatum odit non provident dolore nam dolores blanditiis, impedit in, alias ad reprehenderit cumque commodi necessitatibus corporis.</p>
-   
+    </div>
 </section></div>
+
+<div class="quick-example">
+  <div>
+    this is amazing!
+  </div>
+</div>
+
+<div class="quick-example">
+  <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi corrupti, sint distinctio deserunt vel unde consequatur sequi nobis necessitatibus quis ad officiis doloremque ab, blanditiis facere possimus obcaecati voluptate sed!</p>
+    <p>Voluptas, ipsa porro? Ex nostrum culpa magnam magni officiis laboriosam fugit molestias amet reiciendis sunt facere, debitis odit neque totam natus ad aliquam minus, sed repellat eveniet, et impedit ipsa.</p>
+  </div>
+</div>
+</>
   )
 }
 
