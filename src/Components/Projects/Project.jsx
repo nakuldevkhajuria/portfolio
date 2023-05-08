5dd8261770e63c09d7bd536950d1dc31406b82c4
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-05-08 22:20:34 +0530

    added media queries for responsiveness

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import './Style.css'
 const Project = () => {
   return (
-    <>
+    <div className='project'>
+    <div className='heading'>
+<h3>PORTFOLIO</h3>
+<p>Projects are like fingerprints, each one leaving a distinctive mark on the journey of development.
+<div className='journey'></div>
+</p>
+    </div>
+  
     <div className='section'> 
     <h1>Project 1</h1>
        <section id='grid' className='blue'>
@@ -29,19 +36,7 @@ const Project = () => {
     </div>
 </section></div>
 
-<div class="quick-example">
-  <div>
-    this is amazing!
-  </div>
 </div>
-
-<div class="quick-example">
-  <div>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi corrupti, sint distinctio deserunt vel unde consequatur sequi nobis necessitatibus quis ad officiis doloremque ab, blanditiis facere possimus obcaecati voluptate sed!</p>
-    <p>Voluptas, ipsa porro? Ex nostrum culpa magnam magni officiis laboriosam fugit molestias amet reiciendis sunt facere, debitis odit neque totam natus ad aliquam minus, sed repellat eveniet, et impedit ipsa.</p>
-  </div>
-</div>
-</>
   )
 }
 
