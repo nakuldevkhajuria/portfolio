56e2b47aa11689bdaab77208155c63eac7959866
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-05-19 17:42:10 +0530

    About component added, and teck stack done

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import './Style.css'
 const About = () => {
     return (
-        <div className='red'>
-       gffg
-            
+        <div className='section'>
+   <div className="container">
+    <div className='aboutContent'> 
+    <div id='aboutImage'></div>
+    <div class="text-side">
+        <h4>About me</h4>
+        <h3>A dedicated Front-end Developer <br/> 
+        based in Belgrade, Serbia 📍</h3><p>As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p></div>
+     
+    </div>
+    <div></div>
+   </div>
 
         </div>
     )
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -18,7 +18,7 @@ useEffect(()=>{
                         <i className="fa-solid fa-display"></i>
                         </h1>
                         <p>
-                            Hi, i'm Nakul dev Khajuria. A passionate Front-end React developer based in Delhi, India.
+                            Hi, i'm Nakul dev Khajuria. A passionate MERN Stack developer based in Delhi, India.
                         </p>
                         <div className='social-media'>
                             <a aria-label="linkedin" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/nakuldevkhajuria/"><i className="fa-brands fa-linkedin"></i></a>
@@ -30,17 +30,20 @@ useEffect(()=>{
 
                     </div>
                 </div>
+                <div className='skillsContainer'>
                 <div className='skills'>
                     <p>Tech Stack</p>
                    <ul id='myList'>
-                    <li>f</li>
+                    <li ></li>
+                    <li></li>
+                    <li></li>
                     <li></li>
                     <li></li>
                     <li></li>
                     <li></li>
                   
                    </ul>
-                </div>
+                </div></div>
             </div>
         </div>
   
