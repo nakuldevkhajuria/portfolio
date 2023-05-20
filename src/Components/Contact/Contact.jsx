ac505d4f04d421b72588fcd8bff24203e32028d7
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-05-20 19:47:31 +0530

    added responsive nature to About component.

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import  './Style.css'
+const Contact = () => {
+    return (
+        <div className='contactSection'>
+            <div className='contactContainer'>
+                <div className='contactContent'>
+                    <h3>CONTACT</h3>
+                    <h2>Reach out to start a collaboration, share your thoughts, or simply say hello!</h2>
+                    <div class="contact__icons"><div class="contact__icon-box"><span><i class="fa-solid fa-map-location-dot"></i></span><div class="contact__info"><h3>Location</h3><p>Belgrade, Serbia</p></div></div><div class="contact__icon-box"><span><i class="fa-solid fa-envelope-open-text"></i></span><div class="contact__info"><h3>Mail</h3><a href="mailto:stefan.topallovic@gmail.com">stefan.topallovic@gmail.com</a></div></div></div>
+
+                    {/* <div class="contact__icons">
+                        <div class="contact__icon-box">
+                            <span><i class="fa-solid fa-map-location-dot">
+                                </i>
+                            </span>
+                        <div class="contact__info">
+                    <h3>Location</h3><p>Belgrade, Serbia</p></div></div>
+                    <div class="contact__icon-box">
+                        <span>
+                            <i class="fa-solid fa-envelope-open-text"></i>
+                        </span>
+                    <div class="contact__info">
+                        <h3>Mail</h3><a href="mailto:stefan.topallovic@gmail.com">stefan.topallovic@gmail.com</a></div></div></div> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
